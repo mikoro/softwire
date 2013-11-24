4ed3920baf2c21aaf7f82d5bb6e28f3c158ccab9
Mikko Ronkainen <mikko@mikkoronkainen.com>
2013-11-24 22:41:02 +0200

    Added support for ldc compiler

--- misc/resources/compile.bat
+++ misc/resources/compile.bat
@@ -2,7 +2,7 @@
 rem Needs to be run from VS command line for rc, needs gcc/gdc in path for windres
 
 echo Compiling resource file for DMD
-rc /nologo /fo softwire.dmd.res softwire.rc
+rc /nologo /fo softwire.res softwire.rc
 
 echo Compiling resource file for GDC
-windres softwire.rc -o softwire.gdc.res.o
+windres softwire.rc -o softwire.res.o
