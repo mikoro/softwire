1983f9312721604dbf7f54a9db5c65324483fc34
Mikko Ronkainen <mikko@mikkoronkainen.com>
2013-12-26 23:06:00 +0200

    Re-arranged freetype imports

--- import/derelict/freetype/ft.d
+++ import/derelict/freetype/ft.d
@@ -27,12 +27,21 @@ DEALINGS IN THE SOFTWARE.
 */
 module derelict.freetype.ft;
 
+public import derelict.freetype.types;
+
+extern(C) nothrow
+{
+    FT_Error FT_Init_FreeType(FT_Library*);
+    FT_Error FT_New_Face(FT_Library, const(char)*, FT_Long, FT_Face*);
+    FT_Error FT_Set_Pixel_Sizes(FT_Face, FT_UInt, FT_UInt);
+    FT_Error FT_Load_Char(FT_Face, FT_ULong, FT_Int32);
+}
+
+/*
 public {
     import derelict.freetype.types;
     import derelict.freetype.functions;
 }
-
-/*
 
 private {
     import derelict.util.loader;
